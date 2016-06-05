82d031a98e4c7121fdbc7beaf6ebaaaac45f70b7
supershowwei <supershowwei@gmail.com>
2016-06-05 22:51:08 +0800

    complete linebot samples

--- LineBotSamples/LineBotSamples/Controllers/ChatController.cs
+++ LineBotSamples/LineBotSamples/Controllers/ChatController.cs
@@ -3,37 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
+using Jil;
+using LineBotSamples.SignalR;
+using Microsoft.AspNet.SignalR;
 using RestSharp;
 
 namespace LineBotSamples.Controllers
 {
     public class ChatController : Controller
     {
-        private XDocument xdoc;
-
         // GET: Chat
         public ActionResult Index()
         {
-            ViewBag.ReceivedMessage = "";
-
-            if (System.IO.File.Exists(Path.Combine(Server.MapPath("~/"), "receivedMessage.txt")))
-            {
-                using (StreamReader sr = new StreamReader(Path.Combine(Server.MapPath("~/"), "receivedMessage.txt")))
-                {
-                    ViewBag.ReceivedMessage = sr.ReadToEnd();
-                }
-            }
-
-            return View();
+            return this.View();
         }
 
         [HttpPost]
         public ActionResult Receive()
         {
-            string rawData = "";
+            XDocument xml = XDocument.Load(Server.MapPath("~/App_Data/customers.xml"));
+
+            string rawData = string.Empty;
 
             Request.InputStream.Seek(0, SeekOrigin.Begin);
             using (StreamReader sr = new StreamReader(Request.InputStream))
@@ -41,35 +35,124 @@ namespace LineBotSamples.Controllers
                 rawData = sr.ReadToEnd();
             }
 
-            using (StreamWriter sw = new StreamWriter(Path.Combine(Server.MapPath("~/"), "receivedMessage.txt")))
+            var results = JSON.DeserializeDynamic(rawData).result;
+
+            string customerId = results[0].content.from;
+            string text = results[0].content.text;
+
+            var customer =
+                xml.Root.Elements()
+                    .Where(c => c.Attribute("Id").Value.Equals(customerId))
+                    .SingleOrDefault();
+
+            if (customer == null)
             {
-                sw.Write(rawData);
+                // 客戶不存在就建立客戶資料
+                customer = this.CreateCustomer(customerId, xml);
+
+                this.SendMessage("客人您好，請問該怎麼稱呼您呢？", customer.Attribute("Id").Value);
+            }
+            else if (customer.Attribute("FollowId").Value.Equals("0"))
+            {
+                // 儲存客戶稱呼
+                customer.Attribute("Name").Value = results[0].content.text;
+                customer.Attribute("FollowId").Value = "1";
+
+                this.SaveCustomers(xml);
+
+                this.SendMessage(
+                    $"{customer.Attribute("Name").Value}，您好！請問您住哪裡？",
+                    customer.Attribute("Id").Value);
+            }
+            else if (customer.Attribute("FollowId").Value.Equals("1"))
+            {
+                // 儲存客戶居住地
+                customer.Attribute("Place").Value = results[0].content.text;
+                customer.Attribute("FollowId").Value = "2";
+
+                this.SaveCustomers(xml);
+
+                this.SendMessage(
+                    $"{customer.Attribute("Place").Value}的{customer.Attribute("Name").Value}，您好！請問有什麼可以為您服務的嗎？",
+                    customer.Attribute("Id").Value);
+
+                // 客服人員接起
+                var customerServiceHub = GlobalHost.ConnectionManager.GetHubContext<CustomerServiceHub>();
+
+                customerServiceHub.Clients.All.inbound(
+                    $"{customer.Attribute("Place").Value}的{customer.Attribute("Name").Value}來了，正在線上。",
+                    customer.Attribute("Id").Value);
+            }
+            else if (customer.Attribute("FollowId").Value.Equals("2"))
+            {
+                var customerServiceHub = GlobalHost.ConnectionManager.GetHubContext<CustomerServiceHub>();
+
+                if (text.StartsWith("再見", StringComparison.Ordinal))
+                {
+                    // 客戶道再見
+                    customer.Attribute("FollowId").Value = "0";
+
+                    this.SaveCustomers(xml);
+
+                    customerServiceHub.Clients.All.chat(
+                        "客戶離開！",
+                        customer.Attribute("Id").Value,
+                        customer.Attribute("Place").Value,
+                        customer.Attribute("Name").Value);
+                }
+                else
+                {
+                    // 客服人員與客戶溝通
+                    customerServiceHub.Clients.All.chat(
+                        text,
+                        customer.Attribute("Id").Value,
+                        customer.Attribute("Place").Value,
+                        customer.Attribute("Name").Value);
+                }
             }
 
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
-        public ActionResult Send(string message)
+        public ActionResult Send(string message, string customerId)
         {
-            this.xdoc = XDocument.Load(Path.Combine(Server.MapPath("~/"), "confidential.xml"));
+            this.SendMessage(message, customerId);
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        private XElement CreateCustomer(string id, XDocument xml)
+        {
+            XElement customer =
+                new XElement(
+                    "Customer",
+                    new XAttribute("Id", id),
+                    new XAttribute("Name", string.Empty),
+                    new XAttribute("Place", string.Empty),
+                    new XAttribute("FollowId", 0));
+
+            xml.Root.Add(customer);
+
+            this.SaveCustomers(xml);
+
+            return customer;
+        }
+
+        private void SendMessage(string message, string customerId)
+        {
+            XDocument xml = XDocument.Load(Server.MapPath("~/App_Data/confidential.xml"));
 
             RestClient restClient = new RestClient(@"https://trialbot-api.line.me/v1/events");
             RestRequest restRequest = new RestRequest(Method.POST);
 
             restRequest.AddHeader("Content-Type", "application/json; charser=UTF-8");
-            restRequest.AddHeader("X-Line-ChannelID", this.xdoc.Root.Element("ChannelID").Value);
-            restRequest.AddHeader("X-Line-ChannelSecret", this.xdoc.Root.Element("ChannelSecret").Value);
-            restRequest.AddHeader("X-Line-Trusted-User-With-ACL", this.xdoc.Root.Element("ChannelMID").Value);
-
-            var friends =
-                new[]
-                {
-                    this.GetFriendMID("莊志弘")
-                };
+            restRequest.AddHeader("X-Line-ChannelID", xml.Root.Element("ChannelID").Value);
+            restRequest.AddHeader("X-Line-ChannelSecret", xml.Root.Element("ChannelSecret").Value);
+            restRequest.AddHeader("X-Line-Trusted-User-With-ACL", xml.Root.Element("ChannelMID").Value);
 
             restRequest.AddJsonBody(new
             {
-                to = friends,
+                to = new[] { customerId },
                 toChannel = 1383378250,
                 eventType = "138311608800106203",
                 content = new
@@ -81,16 +164,11 @@ namespace LineBotSamples.Controllers
             });
 
             var restResponse = restClient.Execute(restRequest);
-
-            return View("Index");
         }
 
-        private string GetFriendMID(string name)
+        private void SaveCustomers(XDocument xml)
         {
-            var friendElements = this.xdoc.Root.Element("Friends").Elements();
-            var friendElement = friendElements.Where(f => f.Attribute("Name").Value == name).Single();
-
-            return friendElement.Attribute("MID").Value;
+            xml.Save(Server.MapPath("~/App_Data/customers.xml"));
         }
     }
 }
---
+++ LineBotSamples/LineBotSamples/SignalR/CustomerServiceHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace LineBotSamples.SignalR
+{
+    public class CustomerServiceHub : Hub
+    {
+        public void Chat(string message, string customerId, string customerName, string customerPlace)
+        {
+            Clients.All.chat(message, customerId, customerName, customerPlace);
+        }
+    }
+}
